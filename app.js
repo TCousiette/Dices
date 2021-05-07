0ad0c6ec6391654645f9e376a2d55d81e3e53493
Timur <75565469+TCousiette@users.noreply.github.com>
2021-05-07 17:25:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/dicee.html");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is ok");
+  });
