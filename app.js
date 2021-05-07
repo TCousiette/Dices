1449bd793e8def8c0290642070029813d074a2fa
Timur <75565469+TCousiette@users.noreply.github.com>
2021-05-07 17:34:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/dicee.html");
+  res.sendFile("dicee.html");
 });
 
 app.listen(process.env.PORT || 3000, function(){
